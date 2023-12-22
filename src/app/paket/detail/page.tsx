e7fd90866e8f1ada8e25984ed4c0454c98099985
acepW <acepwahyu001@gmail.com>
2023-12-22 22:55:54 +0700

    add data paket non aktif to tabel

--- src/app/paket/detail/page.tsx
+++ src/app/paket/detail/page.tsx
@@ -18,35 +18,73 @@ function Detail() {
       const res = await axios.get(`http://localhost:5000/api/paket/${idd}`);
       if (res.data.success == true) {
         setDetailPaket(res.data.data);
-
       }
     } catch (error: any) {
       console.log(error.response);
     }
   }
 
+  // submit form paket to url
+  async function editStatus(e: any) {
+    e.preventDefault();
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/api/paket/${id}`,
+        {
+          status: "non_aktif",
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      alert("Paket berhasil di non aktifkan");
+    } catch (error: any) {
+      alert(error.response.data.message);
+    }
+  }
 
-  const date = DetailPaket == null ? new Date() : new Date(DetailPaket.waktu_keberangkatan);
+  const date =
+    DetailPaket == null
+      ? new Date()
+      : new Date(DetailPaket.waktu_keberangkatan);
   // Format tanggal
   const WaktuKeberangkatan = format(date, "d MMM y");
 
   return (
-
     <div className="flex">
       <SideBar paket=" text-white bg-[#E3B02B]" />
       <div className="h-screen w-screen grey px-[28px] py-[20px] overflow-y-scroll">
         <p className="font-semibold text-[28px]">Detail Paket</p>
         <div className="bg-white rounded-[10px] w-4/6 mt-[20px] p-5">
+          {DetailPaket != null && DetailPaket.status == "aktif" ? (
+            <div className="py-2">
+              <button
+                onClick={editStatus}
+                className="w-full font-semibold text-xl  rounded-md p-3 text-semibold text-white bg-red-600 hover:bg-red-500"
+              >
+                Non Aktifkan
+              </button>
+            </div>
+          ) : (
+            <div></div>
+          )}
+
           <p className="font-semibold text-[20px]">Detail</p>
           <div className="w-full py-2 grid grid-cols-4 gap-2 ">
-            {
-              DetailPaket == null ? <div></div> :
-                DetailPaket.content_carousel.map((data: any, i: number) => {
-                  return <img key={i} src={`http://localhost:5000/images/${data.img}`} alt="" className="bg-slate-200 h-20"></img>
-                })
-            }
-
-
+            {DetailPaket == null ? (
+              <div></div>
+            ) : (
+              DetailPaket.content_carousel.map((data: any, i: number) => {
+                return (
+                  <img
+                    key={i}
+                    src={`http://localhost:5000/images/${data.img}`}
+                    alt=""
+                    className="bg-slate-200 h-20"
+                  ></img>
+                );
+              })
+            )}
           </div>
           <div className="flex w-full gap-10">
             <div className="py-3 w-full">
@@ -67,16 +105,16 @@ function Detail() {
               <div className="py-1 ">
                 <p className="text-[18px] font-medium">Jenis Keberangkatan</p>
                 <div className="relative w-full">
-                  <p>{DetailPaket == null ? "" : DetailPaket.jenis_keberangkatan}</p>
+                  <p>
+                    {DetailPaket == null ? "" : DetailPaket.jenis_keberangkatan}
+                  </p>
                   <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
                 </div>
               </div>
               <div className="py-1 mt-5">
                 <p className="text-[18px] font-medium">Deskripsi Paket</p>
                 <div className="relative w-full">
-                  <p>
-                    {DetailPaket == null ? "" : DetailPaket.description}
-                  </p>
+                  <p>{DetailPaket == null ? "" : DetailPaket.description}</p>
                   <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
                 </div>
               </div>
@@ -101,28 +139,38 @@ function Detail() {
               <div className="mt-5">
                 <p className="text-[18px] font-medium">Maskapai Pesawat</p>
                 <div className="relative w-full">
-                  <p>{DetailPaket == null ? "" : DetailPaket.maskapai_penerbangan}</p>
+                  <p>
+                    {DetailPaket == null
+                      ? ""
+                      : DetailPaket.maskapai_penerbangan}
+                  </p>
                   <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
                 </div>
               </div>
               <div className="mt-5">
                 <p className="text-[18px] font-medium">Kota Keberangkatan</p>
                 <div className="relative w-full">
-                  <p>{DetailPaket == null ? "" : DetailPaket.kota_keberangkatan}</p>
+                  <p>
+                    {DetailPaket == null ? "" : DetailPaket.kota_keberangkatan}
+                  </p>
                   <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
                 </div>
               </div>
               <div className="mt-5">
                 <p className="text-[18px] font-medium">Durasi Perjalanan</p>
                 <div className="relative w-full">
-                  <p>{DetailPaket == null ? "" : DetailPaket.durasi_perjalanan}</p>
+                  <p>
+                    {DetailPaket == null ? "" : DetailPaket.durasi_perjalanan}
+                  </p>
                   <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
                 </div>
               </div>
               <div className="mt-5">
                 <p className="text-[18px] font-medium">Kelas Hotel</p>
                 <div className="relative w-full">
-                  <p>{DetailPaket == null ? "" : DetailPaket.rating_hotel + "/5"}</p>
+                  <p>
+                    {DetailPaket == null ? "" : DetailPaket.rating_hotel + "/5"}
+                  </p>
                   <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
                 </div>
               </div>
@@ -135,7 +183,9 @@ function Detail() {
               <div className="">
                 <p className="text-[18px] font-medium">Detail Fasilitas</p>
                 <div className="relative w-full">
-                  <p>{DetailPaket == null ? "" : DetailPaket.fasilitas_umroh}</p>
+                  <p>
+                    {DetailPaket == null ? "" : DetailPaket.fasilitas_umroh}
+                  </p>
                 </div>
               </div>
             </div>
@@ -144,67 +194,77 @@ function Detail() {
           <p className="font-semibold text-[20px] mt-5">Detail Hotel</p>
 
           {/* ARRAY HOTEL */}
-          {
-            DetailPaket == null ? <div></div> :
-              DetailPaket.hotel.map((data: any, i: number) => {
-                return (
-                  <div key={i}>
-                    <p className="pt-2 text-[41px] font-semibold">{i + 1}</p>
-                    <div className="w-full py-2 grid grid-cols-4 gap-2 ">
-                      {
-                        data.content.map((data: any, ii: number) => {
-                          return <img key={ii} src={`http://localhost:5000/images/${data.img}`} alt="" className="bg-slate-200 h-20"></img>
-                        })
-                      }
-
-
-                    </div>
-                    <div className="flex w-full gap-10">
-                      <div className=" w-3/6">
-                        <div className="">
-                          <p className="text-[18px] font-medium">Kota</p>
+          {DetailPaket == null ? (
+            <div></div>
+          ) : (
+            DetailPaket.hotel.map((data: any, i: number) => {
+              return (
+                <div key={i}>
+                  <p className="pt-2 text-[41px] font-semibold">{i + 1}</p>
+                  <div className="w-full py-2 grid grid-cols-4 gap-2 ">
+                    {data.content.map((data: any, ii: number) => {
+                      return (
+                        <img
+                          key={ii}
+                          src={`http://localhost:5000/images/${data.img}`}
+                          alt=""
+                          className="bg-slate-200 h-20"
+                        ></img>
+                      );
+                    })}
+                  </div>
+                  <div className="flex w-full gap-10">
+                    <div className=" w-3/6">
+                      <div className="">
+                        <p className="text-[18px] font-medium">Kota</p>
+                        <div className="relative w-full">
+                          <p>{data.city}</p>
+                          <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
+                        </div>
+                        <div className="mt-5">
+                          <p className="text-[18px] font-medium">Hotel</p>
                           <div className="relative w-full">
-                            <p>{data.city}</p>
+                            <p>{data.name}</p>
                             <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
                           </div>
-                          <div className="mt-5">
-                            <p className="text-[18px] font-medium">Hotel</p>
-                            <div className="relative w-full">
-                              <p>{data.name}</p>
-                              <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
-                            </div>
-                          </div>
-                          <div className="mt-5">
-                            <p className="text-[18px] font-medium">Fasilitas</p>
-                            <div className="w-full">
-                              <div className="flex flex-col mt-1">
+                        </div>
+                        <div className="mt-5">
+                          <p className="text-[18px] font-medium">Fasilitas</p>
+                          <div className="w-full">
+                            <div className="flex flex-col mt-1">
+                              <p>
+                                {data.fasilitas.FreeWiFi == false
+                                  ? ""
+                                  : "Free Wifi"}
+                              </p>
 
-                                <p>{
-                                  data.fasilitas.FreeWiFi == false ? "" : "Free Wifi"
-                                }</p>
+                              <p>
+                                {data.fasilitas.Disabilitas == false
+                                  ? ""
+                                  : "Disabilitas"}
+                              </p>
 
-                                <p>{
-                                  data.fasilitas.Disabilitas == false ? "" : "Disabilitas"
-                                }</p>
+                              <p>
+                                {data.fasilitas.TempatMakan == false
+                                  ? ""
+                                  : "Tempat Makan"}
+                              </p>
 
-                                <p>{
-                                  data.fasilitas.TempatMakan == false ? "" : "Tempat Makan"
-                                }</p>
-
-                                <p>{
-                                  data.fasilitas.PelayananKamar == false ? "" : "Pelayanan Kamar"
-                                }</p>
-                              </div>
+                              <p>
+                                {data.fasilitas.PelayananKamar == false
+                                  ? ""
+                                  : "Pelayanan Kamar"}
+                              </p>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                )
-              })
-          }
-
+                </div>
+              );
+            })
+          )}
 
           {/* END OF ARRAY HOTEL */}
 
@@ -212,22 +272,21 @@ function Detail() {
           <div className="flex w-full gap-10">
             <div className="py-3 w-full">
               {/* AGENDA ARRAY */}
-              {
-                DetailPaket == null ? <div></div> :
-                  DetailPaket.jadwal.map((data: any, i: number) => {
-                    return (
-                      <div key={i}>
-                        <p className="text-[18px] font-medium">{data.hari}</p>
-                        <div className="relative w-full">
-                          <p>{data.agenda}</p>
-                          <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
-                        </div>
+              {DetailPaket == null ? (
+                <div></div>
+              ) : (
+                DetailPaket.jadwal.map((data: any, i: number) => {
+                  return (
+                    <div key={i}>
+                      <p className="text-[18px] font-medium">{data.hari}</p>
+                      <div className="relative w-full">
+                        <p>{data.agenda}</p>
+                        <div className="absolute bottom-0 left-0 w-0 h-0 border-t-2 border-gray-300 border-opacity-50"></div>
                       </div>
-                    )
-                  })
-              }
-
-
+                    </div>
+                  );
+                })
+              )}
 
               {/* END OF AGENDA ARRAY */}
             </div>
@@ -241,7 +300,9 @@ function Detail() {
                   Detail Syarat dan Ketentuan
                 </p>
                 <div className="relative w-full">
-                  <p>{DetailPaket == null ? "" : DetailPaket.syarat_ketentuan}</p>
+                  <p>
+                    {DetailPaket == null ? "" : DetailPaket.syarat_ketentuan}
+                  </p>
                 </div>
               </div>
             </div>
@@ -268,7 +329,10 @@ function Detail() {
             <div className="w-3/6"></div>
           </div>
           <div className="pt-5">
-            <a className="w-full font-semibold text-xl rounded-md p-3 text-semibold text-white bg-[#E3B02B] hover:bg-[#b18a2a]">
+            <a
+              href={`/paket/edit?id=${id}`}
+              className="w-full font-semibold text-xl rounded-md p-3 text-semibold text-white bg-[#E3B02B] hover:bg-[#b18a2a]"
+            >
               Edit Paket
             </a>
           </div>
